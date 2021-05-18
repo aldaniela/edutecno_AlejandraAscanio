368c085ca437647d391a6268e95b2a0441f042e9
Alejandra Ascanio <aldaniela@gmail.com>
2021-05-17 22:01:06 -0400

    eliminando elementos de sass no utilizados

---
+++ desafios_M1/desafio_ricomida-AAscanio/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+
+  })
+
+
+  $("#enviarCorreo").click(function(){
+    alert("Mensaje de alerta")
+    })
+
+    $('.carousel').carousel({
+        interval: false
+      })
+    $("#enviarCorreo").click(function(){
+        alert("Mensaje de alerta");
+        }
+    $('[data-toggle="popover"]').popover()
