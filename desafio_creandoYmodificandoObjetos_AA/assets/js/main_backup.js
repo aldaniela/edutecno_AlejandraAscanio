7d7f7cbbf83ae27a2fbdcd2f10f174fb8cef1f39
Alejandra Ascanio <aldaniela@gmail.com>
2021-06-18 13:41:44 -0400

    agregar y validar opcion para editar datos del paciente

--- desafio_creandoYmodificandoObjetos_AA/assets/js/main_backup.js
+++ desafio_creandoYmodificandoObjetos_AA/assets/js/main_backup.js
@@ -8,10 +8,6 @@ Consultorio.prototype.getPacientes = function() {
     return this.paciente;
 };
 //Para obtener cada dato del paciente por separado
-Consultorio.prototype.getNombreCons = function(i) {
-    return this.nombre;
-};
-//Para obtener cada dato del paciente por separado
 Consultorio.prototype.getNombre = function(i) {
     return this.paciente[i].nombre;
 };
@@ -48,7 +44,6 @@ Paciente.prototype.setRUT = function(rut) {
 Paciente.prototype.setDiagnostico = function(diagnostico) {
     this.diagnostico = diagnostico;
 };
-
 //Carga previa de datos para prueba
 var pac1 = new Paciente("Daniela", 37, "26155264-7", "tos");
 var pac2 = new Paciente("Maria", 70, "27119327-0", "asma");
@@ -63,11 +58,11 @@ pac3.setRUT("75321548-7");
 pac1.setDiagnostico("catarro");
 consu1.paciente[1].setNombre("Nuevo Nombre")
 console.log(consu1.getNombre(1))
-consu1.nombre = "Cardiologia";
+consu1.nombre = "Otro consultorio";
 console.log(`Nombre del consultorio es: ${consu1.nombre}`)
 
 
-//Verificar que el formulario para agregar no esté vacio
+//Verificar que el formunlario para agregar no esté vacio
 var validar_form = (nombre, edad, rut, diagnostico) => {
     if (nombre == "" || edad == "" || rut == "" || diagnostico == "") {
         return false;
@@ -79,9 +74,9 @@ var validar_form = (nombre, edad, rut, diagnostico) => {
 
 $(document).ready(function() {
     //Identificar el consultorio
-    $("#consultorio").text(`Consultorio de ${consu1.getNombreCons()}`);
+    $("#consultorio").text(`Consultorio de ${consu1.nombre}`);
     //Accion para ingresar un paciente
-    $('#ingresar').click(function(e) {
+    $('#button').click(function(e) {
         e.preventDefault();
         let nombre = $("#nombre").val();
         let edad = $("#edad").val();
@@ -90,11 +85,6 @@ $(document).ready(function() {
         if (validar_form(nombre, edad, rut, diagnostico)) {
             let nuevo_paciente = new Paciente(nombre, edad, rut, diagnostico);
             consu1.setPacientes(nuevo_paciente);
-            alert("Nuevo paciente ingresado con éxito.")
-            $("#nombre").val("");
-            $("#edad").val("");
-            $("#rut").val("");
-            $("#diagnostico").val("");
         } else {
             alert("El formulario debe estar completo.")
         }
@@ -123,43 +113,6 @@ $(document).ready(function() {
             $('#fila' + j).html(`<td>${consu1.getNombre(i)}</td><td>${consu1.getEdad(i)}</td><td>${consu1.getRut(i)}</td><td>${consu1.getDiagnostico(i)}</td>`);
             $('#body_tabla').append('<tr id="fila' + (j + 1) + '"></tr>');
             j++;
-        }
-    });
-    $('#editar').click(function(e) {
-        e.preventDefault();
-        let nombre_editar = $("#nombre_editar").val();
-        for (i = 0; i < consu1.paciente.length; i++) {
-            if (nombre_editar == consu1.getNombre(i)) {
-                $("#nombre_a_editar").text(`A continuación los datos del paciente: ${nombre_editar}`);
-                $("#form_a_editar").show();
-                $("#edad_editar").val(consu1.getEdad(i));
-                $("#rut_editar").val(consu1.getRut(i));
-                $("#diagnostico_editar").val(consu1.getDiagnostico(i));
-                indice_a_editar = i;
-                i = consu1.paciente.length; //Para poder salir del for
-            } else {
-                $("#nombre_a_editar").text("Paciente no encontrado");
-            }
-        }
-    });
-    $('#enviar_editar').click(function(e) {
-        e.preventDefault();
-        let nombre = $("#nombre_editar").val();
-        let edad = $("#edad_editar").val();
-        let rut = $("#rut_editar").val();
-        let diagnostico = $("#diagnostico_editar").val();
-        if (validar_form(nombre, edad, rut, diagnostico)) {
-            consu1.paciente[indice_a_editar].setEdad(edad);
-            consu1.paciente[indice_a_editar].setRUT(rut);
-            consu1.paciente[indice_a_editar].setDiagnostico(diagnostico);
-            alert("Datos modificados con éxito");
-            $("#nombre_editar").val("");
-            $("#edad_editar").val("");
-            $("#rut_editar").val("");
-            $("#diagnostico_editar").val("");
-            $("#form_a_editar").hide();
-        } else {
-            alert("El formulario debe estar completo.")
         }
     });
 });
