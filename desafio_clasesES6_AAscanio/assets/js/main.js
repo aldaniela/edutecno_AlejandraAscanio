4a9f03bd819edb58d23bf672b46250db90a35c1e
Alejandra Ascanio <aldaniela@gmail.com>
2021-06-21 16:10:58 -0400

    agregando las configuraciones de babel

--- desafio_clasesES6_AAscanio/assets/js/main.js
+++ desafio_clasesES6_AAscanio/assets/js/main.js
@@ -1,17 +1,12 @@
 //Importar la clase Cliente
 
-import Cliente from "../clases/cliente.js"
+/* import Cliente from "../clases/cliente.js" */
+
+import Cliente from "../fullstack-entorno/dist/cliente.js"
 
 //Importar la clase Impuestos
-import Impuestos from "../clases/impuestos.js"
-
-//Prueba de las clases
-var i1 = new Impuestos(1500, 50);
-var c1 = new Cliente("Pedro", i1);
-console.log(c1.nombre);
-console.log(c1);
-var temp = c1.calcularImpuesto(c1.impuesto.monto_bruto_anual, c1.impuesto.deducciones);
-console.log(temp);
+/* import Impuestos from "../clases/impuestos.js" */
+import Impuestos from "../fullstack-entorno/dist/impuestos.js"
 
 //Creo un arreglo para guardar a todos los clientes
 var clientes = [];
@@ -29,7 +24,7 @@ $(document).ready(function() {
             clientes.push(new Cliente(temp_nombre, temp_impuesto));
             let temp_pagar = nuevo_cliente.calcularImpuesto(nuevo_cliente.impuesto.monto_bruto_anual, nuevo_cliente.impuesto.deducciones);
             $("#impuesto_pagar").text(`El impuesto a cancelar es de: ${temp_pagar}`)
-            console.log(clientes);
+            console.log(clientes); //para ir viendo los clientes agregados
         } else {
             alert("El formulario debe estar completo")
         }
