3822a3d978262f6a8e549c1c0e5bc5e6bbc56526
Alejandra Ascanio <aldaniela@gmail.com>
2021-06-22 12:25:45 -0400

    transpilando el archivo main

---
+++ desafio_clasesES6_AAscanio/assets/fullstack-entorno/dist/main.js
@@ -0,0 +1,28 @@
+//Importar la clase Cliente
+import Cliente from "./cliente.js";
+
+//Importar la clase Impuestos
+import Impuestos from "./impuestos.js";
+
+//Creo un arreglo para guardar a todos los clientes
+var clientes = [];
+
+//leyendo los datos y calculando el impuesto
+$(document).ready(function () {
+    $("#calcular_impuesto").click(function (e) {
+        e.preventDefault();
+        let temp_nombre = $("#nombre_cliente").val();
+        let temp_monto_anual = $("#monto_anual_bruto").val();
+        let temp_deducciones = $("#deducciones").val();
+        if (temp_nombre && temp_monto_anual && temp_deducciones) {
+            let temp_impuesto = new Impuestos(temp_monto_anual, temp_deducciones);
+            let nuevo_cliente = new Cliente(temp_nombre, temp_impuesto);
+            clientes.push(new Cliente(temp_nombre, temp_impuesto));
+            let temp_pagar = nuevo_cliente.calcularImpuesto(nuevo_cliente.impuesto.monto_bruto_anual, nuevo_cliente.impuesto.deducciones);
+            $("#impuesto_pagar").text(`El impuesto a cancelar es de: ${temp_pagar}`);
+            console.log(clientes); //para ir viendo los clientes agregados
+        } else {
+            alert("El formulario debe estar completo");
+        }
+    });
+});
